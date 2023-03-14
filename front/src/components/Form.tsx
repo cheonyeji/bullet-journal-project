92c583f04703f53ea88be402e55d47fcf976f242
cheonyeji <ssaltuck@gmail.com>
2023-03-14 21:45:16 +0900

    Add react-hook-form

--- front/src/components/Form.tsx
+++ front/src/components/Form.tsx
@@ -1,35 +1,27 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface InterfaceForm {
+  type: string;
+  content: string;
+}
 
 function Form() {
-  const [type, setType] = useState("");
-  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setType(event.target.value);
-  };
-  const [content, setContent] = useState("");
-  const handleContentInput = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setContent(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(type);
-    console.log(content);
+  const { register, handleSubmit, setValue } = useForm<InterfaceForm>();
+  const handleValid = (data: InterfaceForm) => {
+    console.log(data.type, data.content);
+    setValue("content", "");
   };
   return (
-    <form onSubmit={onSubmit}>
-      <select name="type" onChange={handleSelect} value={type}>
+    <form onSubmit={handleSubmit(handleValid)}>
+      <select {...register("type")}>
         <option value="diet">🍚</option>
         <option value="exercise">💪</option>
         <option value="habitsTodo">✅</option>
         <option value="memo">💬</option>
       </select>
       <input
-        onChange={handleContentInput}
+        {...register("content", { required: true })}
         type="text"
-        name="content"
-        value={content}
         placeholder="Write anything you want"
       />
       <button type="submit">✔</button>
