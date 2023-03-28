aca82de8aef92990fd381eed9220a5f6d95b268f
cheonyeji <ssaltuck@gmail.com>
2023-03-28 23:58:46 +0900

    todo <-> done 구현

--- front/src/components/List.tsx
+++ front/src/components/List.tsx
@@ -1,10 +1,23 @@
-import { InterfaceContent } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { contentState, InterfaceContent } from "../atoms";
 
-function List({ text, state }: InterfaceContent) {
+function List({ text, state, type, id }: InterfaceContent) {
+  const setContents = useSetRecoilState(contentState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setContents((prevContents) => {
+      const targetIndex = prevContents.findIndex(
+        (content) => content.id === id
+      );
+      const newContent = { text, id, type, state: name as any };
+      return [
+        ...prevContents.slice(0, targetIndex),
+        newContent,
+        ...prevContents.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
