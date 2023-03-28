55dc36f3d497f0bccdb0385995aed31d5f826b83
cheonyeji <ssaltuck@gmail.com>
2023-03-28 23:58:00 +0900

    컴포넌트 이동

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import "./App.css";
 import Form from "./components/Form";
-import DietPage from "./pages/DietPage";
-import ExercisePage from "./pages/ExercisePage";
-import HabitsToDoPage from "./pages/HabitsToDoPage";
-import MemoPage from "./pages/MemoPage";
 
 function App() {
   return (
     <>
       <p className="text-3xl font-bold"> Today </p>
       <Form />
-      <DietPage />
-      <ExercisePage />
-      <HabitsToDoPage />
-      <MemoPage />
     </>
   );
 }
