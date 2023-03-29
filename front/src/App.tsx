8c6a12b98f0a13d20527f8e0daa7c9149c7250f9
cheonyeji <ssaltuck@gmail.com>
2023-03-29 19:02:02 +0900

    날짜 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,19 @@ import "./App.css";
 import Form from "./components/Form";
 
 function App() {
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
   return (
     <>
-      <p className="text-3xl font-bold"> Today </p>
+      <div>
+        <span className="text-3xl font-bold"> {dateString} </span>
+        <span className="text-xl text-gray-400"> {dayName} </span>
+      </div>
       <Form />
     </>
   );
