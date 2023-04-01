b6c393123c3647fd01baece83e0f921255d63a01
cheonyeji <ssaltuck@gmail.com>
2023-04-01 22:52:23 +0900

    - 불필요한 selector 모두 제거 - 만약 type에 해당하는 value값이 존재하는 경우에만 List 컴포넌트 렌더링

--- front/src/pages/TodayPage.tsx
+++ front/src/pages/TodayPage.tsx
@@ -1,17 +1,10 @@
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import {
-  Types,
-  contentState,
-  dietSelector,
-  exerciseSelector,
-  habitsTodoSelector,
-  memoSelector,
-} from "../atoms";
+import { useRecoilState } from "recoil";
+import { contentState } from "../atoms";
 import List from "../components/List";
 
 function TodayPage() {
-  const setContents = useSetRecoilState(contentState);
+  const [contents, setContents] = useRecoilState(contentState);
   const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
     console.log(destination, source, draggableId);
     if (!destination) return;
@@ -19,12 +12,6 @@ function TodayPage() {
       // same type movement
       setContents((allContents) => {
         //const contentsCopy = [...allContents[source.droppableId]];]
-        const contentsCopy = allContents.filter(
-          (content) => content.type === source.droppableId
-        );
-        const targetContent = contentsCopy[source.index];
-        contentsCopy.splice(source.index, 1);
-        contentsCopy.splice(destination.index, 0, targetContent);
         return allContents;
       });
     }
@@ -36,33 +23,20 @@ function TodayPage() {
       });
     }
   };
-  const dietContents = useRecoilValue(dietSelector);
-  const exerciseContents = useRecoilValue(exerciseSelector);
-  const habitsTodoContents = useRecoilValue(habitsTodoSelector);
-  const memoContents = useRecoilValue(memoSelector);
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="overflow-y-auto flex-1">
-        {dietContents.length !== 0 && (
-          <List title="🍚 Diet" contents={dietContents} listId={Types.DIET} />
-        )}
-        {exerciseContents.length !== 0 && (
-          <List
-            title="💪 Exercise"
-            contents={exerciseContents}
-            listId={Types.EXERCISE}
-          />
-        )}
-        {habitsTodoContents.length !== 0 && (
-          <List
-            title="✅ Habits & To Do"
-            contents={habitsTodoContents}
-            listId={Types.HABITS_TODO}
-          />
-        )}
-        {memoContents.length !== 0 && (
-          <List title="💬 Memo" contents={memoContents} listId={Types.MEMO} />
+        {Object.keys(contents).map(
+          (type, index) =>
+            contents[type].length !== 0 && (
+              <List
+                title={type}
+                contents={contents[type]}
+                listId={type}
+                key={index}
+              />
+            )
         )}
       </div>
     </DragDropContext>
