fc676e48118edc9a95869ab63c452b3351abeb87
cheonyeji <ssaltuck@gmail.com>
2023-04-01 22:49:20 +0900

    - 따로 type값을 저장하는 atom 선언 - enum 제거 - 최초 렌더링 시 contents 셋팅해주는 selector 작성

--- front/src/atoms.tsx
+++ front/src/atoms.tsx
@@ -1,12 +1,5 @@
 import { atom, selector } from "recoil";
 
-export enum Types {
-  "DIET" = "DIET",
-  "EXERCISE" = "EXERCISE",
-  "HABITS_TODO" = "HABITS_TODO",
-  "MEMO" = "MEMO",
-}
-
 export enum States {
   "TODO" = "TODO",
   "DONE" = "DONE",
@@ -16,43 +9,30 @@ export interface InterfaceContent {
   text: string;
   id: number;
   state: States;
-  type: Types;
+  type: string;
 }
 
-export const contentState = atom<InterfaceContent[]>({
+type InterfaceContentState = {
+  [key in string]: InterfaceContent[];
+};
+
+export const typesState = atom<string[]>({
+  key: "type",
+  default: ["TASK", "DIET", "EXERCISE", "HABITS_TODO", "MEMO"],
+});
+
+export const contentState = atom<InterfaceContentState>({
   key: "content",
-  default: [],
+  default: {},
 });
 
-// 추후 유저의 요구에 따라 분류type을 추가할 수 있게 되면 변경 필요한 부분
-export const dietSelector = selector({
-  key: "dietSelector",
-  get: ({ get }) => {
-    const contents = get(contentState);
-    return contents.filter((content) => content.type === Types.DIET);
-  },
-});
-
-export const exerciseSelector = selector({
-  key: "exerciseSelector",
-  get: ({ get }) => {
-    const contents = get(contentState);
-    return contents.filter((content) => content.type === Types.EXERCISE);
-  },
-});
-
-export const habitsTodoSelector = selector({
-  key: "habitsTodoSelector",
-  get: ({ get }) => {
-    const contents = get(contentState);
-    return contents.filter((content) => content.type === Types.HABITS_TODO);
-  },
-});
-
-export const memoSelector = selector({
-  key: "memoSelector",
-  get: ({ get }) => {
-    const contents = get(contentState);
-    return contents.filter((content) => content.type === Types.MEMO);
+export const contentWithTypeSelector = selector({
+  key: "contentWithTypeSelector",
+  get: () => {},
+  set: ({ get, set }) => {
+    const types = get(typesState);
+    const newContent: InterfaceContentState = {};
+    types.map((type) => (newContent[type] = []));
+    set(contentState, newContent);
   },
 });
