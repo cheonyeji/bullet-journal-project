11c6955c53848521d345ed99c002115826d32d1c
cheonyeji <ssaltuck@gmail.com>
2023-04-01 17:23:39 +0900

    순서변경중. 로직 상 atom 수정이 필요하여 브랜치 분기 후 처리해볼 예정

--- front/src/pages/TodayPage.tsx
+++ front/src/pages/TodayPage.tsx
@@ -1,6 +1,8 @@
-import { DragDropContext } from "react-beautiful-dnd";
-import { useRecoilValue } from "recoil";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import {
+  Types,
+  contentState,
   dietSelector,
   exerciseSelector,
   habitsTodoSelector,
@@ -9,25 +11,58 @@ import {
 import List from "../components/List";
 
 function TodayPage() {
-  const onDragEnd = () => {};
+  const setContents = useSetRecoilState(contentState);
+  const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
+    console.log(destination, source, draggableId);
+    if (!destination) return;
+    if (destination.droppableId === source.droppableId) {
+      // same type movement
+      setContents((allContents) => {
+        //const contentsCopy = [...allContents[source.droppableId]];]
+        const contentsCopy = allContents.filter(
+          (content) => content.type === source.droppableId
+        );
+        const targetContent = contentsCopy[source.index];
+        contentsCopy.splice(source.index, 1);
+        contentsCopy.splice(destination.index, 0, targetContent);
+        return allContents;
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // different type(cross) movement
+      setContents((allContents) => {
+        console.log(allContents);
+        return allContents;
+      });
+    }
+  };
   const dietContents = useRecoilValue(dietSelector);
   const exerciseContents = useRecoilValue(exerciseSelector);
   const habitsTodoContents = useRecoilValue(habitsTodoSelector);
   const memoContents = useRecoilValue(memoSelector);
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="overflow-y-auto flex-1">
         {dietContents.length !== 0 && (
-          <List title="🍚 Diet" contents={dietContents} />
+          <List title="🍚 Diet" contents={dietContents} listId={Types.DIET} />
         )}
         {exerciseContents.length !== 0 && (
-          <List title="💪 Exercise" contents={exerciseContents} />
+          <List
+            title="💪 Exercise"
+            contents={exerciseContents}
+            listId={Types.EXERCISE}
+          />
         )}
         {habitsTodoContents.length !== 0 && (
-          <List title="✅ Habits & To Do" contents={habitsTodoContents} />
+          <List
+            title="✅ Habits & To Do"
+            contents={habitsTodoContents}
+            listId={Types.HABITS_TODO}
+          />
         )}
         {memoContents.length !== 0 && (
-          <List title="💬 Memo" contents={memoContents} />
+          <List title="💬 Memo" contents={memoContents} listId={Types.MEMO} />
         )}
       </div>
     </DragDropContext>
