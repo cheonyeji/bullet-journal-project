c4c0bd665782f4850a172f37f022ac8ba9d87565
cheonyeji <ssaltuck@gmail.com>
2023-04-02 11:51:02 +0900

    드래그앤드롭시 contentState 업데이트 (같은 type & type 변경 모두 O)

--- front/src/pages/TodayPage.tsx
+++ front/src/pages/TodayPage.tsx
@@ -5,21 +5,46 @@ import List from "../components/List";
 
 function TodayPage() {
   const [contents, setContents] = useRecoilState(contentState);
-  const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
-    console.log(destination, source, draggableId);
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    console.log(destination, source);
     if (!destination) return;
     if (destination.droppableId === source.droppableId) {
       // same type movement
       setContents((allContents) => {
-        //const contentsCopy = [...allContents[source.droppableId]];]
-        return allContents;
+        const contentsCopy = [...allContents[source.droppableId]];
+        const contentObj = contentsCopy[source.index];
+
+        contentsCopy.splice(source.index, 1);
+        contentsCopy.splice(destination.index, 0, contentObj);
+        const result = {
+          ...allContents,
+          [source.droppableId]: contentsCopy,
+        };
+        console.log(result);
+        return result;
       });
     }
     if (destination.droppableId !== source.droppableId) {
       // different type(cross) movement
       setContents((allContents) => {
-        console.log(allContents);
-        return allContents;
+        const sourceContentsCopy = [...allContents[source.droppableId]];
+        const destinationContentsCopy = [
+          ...allContents[destination.droppableId],
+        ];
+        const contentObj = {
+          ...sourceContentsCopy[source.index],
+          type: destination.droppableId,
+        };
+
+        sourceContentsCopy.splice(source.index, 1);
+        destinationContentsCopy.splice(destination.index, 0, contentObj);
+        const result = {
+          ...allContents,
+          [source.droppableId]: sourceContentsCopy,
+          [destination.droppableId]: destinationContentsCopy,
+        };
+        console.log(result);
+        return result;
       });
     }
   };
