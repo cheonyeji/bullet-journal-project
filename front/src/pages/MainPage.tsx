e93fda6535d6a5a23f9b73cd095769a59b52f893
cheonyeji <ssaltuck@gmail.com>
2023-04-02 16:26:52 +0900

    date atom의 날짜 렌더링하도록 수정

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,20 +1,28 @@
+import { useRecoilValue } from "recoil";
 import Form from "../components/Form";
+import { dateState } from "../atoms";
+import DateBeforeButton from "../components/DateBeforeButton";
+import DateAfterButton from "../components/DateAfterButton";
 
 function MainPage() {
-  const today = new Date();
-  const dateString = today.toLocaleDateString("ko-KR", {
+  const date = useRecoilValue(dateState);
+  const dateString = date.toLocaleDateString("ko-KR", {
     year: "numeric",
     month: "long",
     day: "numeric",
   });
-  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+  const dayName = date.toLocaleDateString("ko-KR", { weekday: "long" });
   return (
-    <div className="bg-white min-w-fit w-3/6 h-3/4 rounded-lg p-3 flex flex-col">
-      <div className="border-b border-b-gray-300 pb-1">
-        <span className="text-3xl font-bold"> {dateString} </span>
-        <span className="text-xl text-gray-400"> {dayName} </span>
+    <div className="bg-zinc-300 w-screen h-screen flex items-center justify-center">
+      <DateBeforeButton />
+      <div className="bg-white min-w-fit w-3/6 h-3/4 rounded-lg p-3 flex flex-col">
+        <div className="border-b border-b-gray-300 pb-1">
+          <span className="text-3xl font-bold"> {dateString} </span>
+          <span className="text-xl text-gray-400"> {dayName} </span>
+        </div>
+        <Form />
       </div>
-      <Form />
+      <DateAfterButton />
     </div>
   );
 }
